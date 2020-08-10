98b8fe4e976c92fd0f057433aa51e3e7a6ef3d8e
eymenkara <eymenkara@gmail.com>
2020-08-10 16:42:01 +0300

    Card Game upload
    Card Images are stored in the local folder

---
+++ Card Game/Card GameTests/Card_GameTests.swift
@@ -0,0 +1,36 @@
+//
+//  Card_GameTests.swift
+//  Card GameTests
+//
+//  Created by Sezer Kara on 11.01.2015.
+//  Copyright (c) 2015 Eymen Kara. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Card_GameTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
